85b94ddf1bbe7da5c008ccf4dec374ff9baa0128
denysmarakhovskyi <109167976+denysmarakhovskyi@users.noreply.github.com>
2023-02-28 11:26:45 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,33 @@
+my_tuple = (1, 10, 100, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
+
+
+print()
+
+
+my_tuple = (1, 10, 100)
+
+t1 = my_tuple + (1000, 10000)
+t2 = my_tuple * 3
+
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(-10 not in my_tuple)
+
+
+print()
+
+
+tup = 1, 2, 3, 2, 4, 5, 6, 2, 7, 2, 8, 9
+duplicates = tup.count(2)
+
+print(duplicates)    # outputs: 4
